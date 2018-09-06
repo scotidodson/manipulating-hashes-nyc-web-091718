7ad90e0baea3ddf5b14788bed275863c7749597d
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 20:55:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts.each do |person, data|
     data.each do |type, info|
-      if(type == favorite_icecream_flavors)
+      if(type == "favorite_icecream_flavors")
        favorite_icecream_flavors.delete_if("strawberry")
      end
    end
